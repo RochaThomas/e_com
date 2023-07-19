accf6e5a0ae3b8d0c28645b68c68f1d7951a920b
RochaThomas <tomrocha99@gmail.com>
2023-07-19 14:02:54 -0700

    cart page built

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './cartStyles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
 
@@ -15,8 +16,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
-                        {/* <CartItem /> */}
+                        <CartItem item={item}/>
                     </Grid>
                 ))}
             </Grid>
@@ -36,7 +36,7 @@ const Cart = ({ cart }) => {
         <div>
             <Container>
                 <div className={classes.toolbar}/>
-                <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+                <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
                 { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
             </Container>
         </div>
